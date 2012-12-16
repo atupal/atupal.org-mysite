73da2596f4b082fb08d4a489da6a9dbceee6d187
atupal <kangleyu@hustunique.com>
2012-12-17 03:23:47 +0800

    l

--- diy/application.py
+++ diy/application.py
@@ -77,8 +77,8 @@ def nimei():
 	fi.close() #此处必须要close，不然会造成ast（抽象语法分析树，即源代码）仍停留在缓存当中，找成编译的时候找不到main函数入口等奇葩的错误
 	fi = open('in.dat', 'w')
 	fi.write(request.form['input'])
+	fi.write('\r\n')
 	fi.close()
-	del fi
 	#tmp = os.popen(cmd)
 	#tmp = os.popen('g++ -c ni.cpp')
 	p = subprocess.Popen(['g++', 'ni.cpp'])
@@ -87,7 +87,7 @@ def nimei():
 	if p.returncode != 0 :
 		return stderrdata
 
-	p = subprocess.Popen(['./a.out','<in.dat'], shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+	p = subprocess.Popen(['./a.out<in.dat'], shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
 	p.wait()
 	stdoutdata, stderrdata = p.communicate()
 	if p.returncode != 0 :
