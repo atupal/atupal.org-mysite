accd5a32b72e910b0e6404a198d1d27c53f6e3c0
atupal <kangleyu@hustunique.com>
2012-12-21 18:39:23 +0800

    reverse

--- diy/application.py
+++ diy/application.py
@@ -110,12 +110,14 @@ import re
 @application.route('/applogs', methods = ['POST', 'GET'])
 def applogs():
 	fi = open('/var/lib/openshift/d06c01f430bd4b308790e4e01b409d6a/diy-0.1/logs/app.log', 'r')
-	re = r'logs + <hr/>'
+	re = 'logs' + r'<hr/>'
 	logs = fi.read()
 	logs = logs.replace('<', '&lt;')
 	logs = logs.replace('>', '&gt;')
 	logs = logs.replace('\n', r'<br/>')
 	logs = logs.split('*** Starting uWSGI')
+	#logs[::-1]
+	reduce(lambda x,y:y+x,logs)
 	for log in logs:
 		re += log + r'<hr/>'
 	return re
