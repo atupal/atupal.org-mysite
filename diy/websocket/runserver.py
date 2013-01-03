05be33cf1cf2174a20bc3579b0127be4e4e536d2
atupal <kangleyu@hustunique.com>
2013-01-03 15:45:51 +0800

    websocket

---
+++ diy/websocket/runserver.py
@@ -0,0 +1,12 @@
+
+#!/usr/bin/env python
+# coding: utf-8
+from gevent.pywsgi import WSGIServer
+from geventwebsocket.handler import WebSocketHandler
+
+from app import my_app
+
+if __name__ == '__main__':
+    http_server = WSGIServer(('127.9.114.1',8080), my_app, handler_class=WebSocketHandler)
+    http_server.serve_forever()
+
