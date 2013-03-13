c4c2cc890cfcb48c6325c649cb7330830cc3a5e1
atupal <kangleyu@hustunique.com>
2013-03-13 19:43:37 +0800

    reboot server

--- diy/runserver.py
+++ diy/runserver.py
@@ -3,5 +3,5 @@ from app import application
 
 from geventwebsocket.handler import WebSocketHandler
 from gevent.pywsgi import WSGIServer
-http_server = WSGIServer(('127.9.114.1',8080), application, handler_class=WebSocketHandler)
+http_server = WSGIServer(('127.9.114.1', 8080), application, handler_class=WebSocketHandler)
 http_server.serve_forever()
