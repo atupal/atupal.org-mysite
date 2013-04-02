b4fe8f47b3c5b86c4379e59ecc783d9871a7596a
atupal <kangleyu@hustunique.com>
2013-04-02 12:15:53 +0800

    请提前确保你在应用文件中的任何 app.run() 调用在 if __name__ == __main__: 块中或是移到一个独立的文件。这是因为它总会启动一个本地 的 WSGI 服务器，并且我们在部署应用到 uWSGI 时不需要它. nginx

--- diy/runserver.py
+++ diy/runserver.py
@@ -3,5 +3,6 @@ from application import app
 
 from geventwebsocket.handler import WebSocketHandler
 from gevent.pywsgi import WSGIServer
-http_server = WSGIServer(('127.9.114.1', 8080), app, handler_class=WebSocketHandler)
-http_server.serve_forever()
+if __name__ == "__main__":
+    http_server = WSGIServer(('127.9.114.1', 8080), app, handler_class=WebSocketHandler)
+    http_server.serve_forever()
