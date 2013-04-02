1cc14961e96c78b2eb924f912686f5571da00c6b
atupal <kangleyu@hustunique.com>
2013-04-02 16:02:57 +0800

    config nginx to process the static file:js, css, img, and so on

--- diy/application/others/base.py
+++ diy/application/others/base.py
@@ -14,31 +14,31 @@ def psb():
 server_dir = '/var/lib/openshift/d06c01f430bd4b308790e4e01b409d6a/app-root/runtime/repo/diy/application/'
 if (os.environ['HOME'] == '/home/atupal'):
     server_dir = './application/'
-@app.route('/js/<path:name>')
-def javascript(name):
-    fi = open(server_dir + "static/js/" + name)
-    return fi.read()
-
-@app.route('/images/<path:name>')
-def images(name):
-    fi = open(server_dir + "static/images/" + name)
-    return fi.read()
-
-@app.route('/css/<path:name>')
-def style(name):
-    fi = open(server_dir + "static/css/" + name)
-    return fi.read()
-
-@app.route('/favicon.ico')
-def ico():
-    fi = open(server_dir + 'favicon.ico', "rb")
-    return fi.read()
-
-@app.route('/static/<path:name>')
-def staticfile(name):
-    fi = open(server_dir + "static/" + name)
-    return fi.read()
-
+#@app.route('/js/<path:name>')
+#def javascript(name):
+#    fi = open(server_dir + "static/js/" + name)
+#    return fi.read()
+#
+#@app.route('/images/<path:name>')
+#def images(name):
+#    fi = open(server_dir + "static/images/" + name)
+#    return fi.read()
+#
+#@app.route('/css/<path:name>')
+#def style(name):
+#    fi = open(server_dir + "static/css/" + name)
+#    return fi.read()
+#
+#@app.route('/favicon.ico')
+#def ico():
+#    fi = open(server_dir + 'favicon.ico', "rb")
+#    return fi.read()
+#
+#@app.route('/static/<path:name>')
+#def staticfile(name):
+#    fi = open(server_dir + "static/" + name)
+#    return fi.read()
+#
 @app.route('/getPic', methods = ['GET', 'POST'])
 def getPic():
     startIndex = request.args.get('startIndex', '')
