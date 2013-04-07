db9428c166832ec2b72c31d04927560b2475a176
atupal <kangleyu@hustunique.com>
2013-04-08 00:01:08 +0800

    init clip aceeditor

---
+++ diy/application/static/js/views/clip.js
@@ -0,0 +1,13 @@
+
+clip = function() {
+    ace_url = 'http://d1n0x3qji82z53.cloudfront.net/src-min-noconflict/ace.js'
+    $.getScript(ace_url, function(data, textStatus, jqxhr){
+        console.log(textStatus);
+        var editor = ace.edit('editor');
+        editor.setTheme('ace/theme/monokai');
+        editor.getSession().setMode('ace/mode/c_cpp');
+    })
+    .fail(function(jqxhr, settings, exception){
+        console.log(exception);
+    });
+}
