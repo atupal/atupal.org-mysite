0cc13b6d05a6492bd5e43bd16a6ae4fc6dd47c43
atupal <kangleyu@hustunique.com>
2013-04-09 00:55:57 +0800

    python-markdown

--- diy/application/views/blog.py
+++ diy/application/views/blog.py
@@ -3,10 +3,13 @@ from application import app
 import markdown
 from flask import render_template
 from flask import Markup
+from flaskext.markdown import Markdown
+Markdown(app)
 
 
 @app.route("/blog")
 def blog():
     content = open(app.config['APPLICATION_ROOT_DIR'] + 'tests/test_post.markdown', 'r').read()
-    content = Markup(markdown.markdown(content))
-    return render_template('views/blog.html', **locals())
+    return render_template('views/blog.html', content = content)
+    #content = Markup(markdown.markdown(content))
+    #return render_template('views/blog.html', **locals())
