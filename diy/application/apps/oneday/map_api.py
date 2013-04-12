400005c3474dc0a94a3d726ddc3c89d9e75499b5
atupal <kangleyu@hustunique.com>
2013-04-12 20:37:54 +0800

    change api getLine to two son api : getLine and getPlace

--- diy/application/apps/oneday/map_api.py
+++ diy/application/apps/oneday/map_api.py
@@ -35,11 +35,14 @@ def getLineInfo():
     ret = dict()
     for i in xrange(len(line)):
         tmp = getInfo.getInfo(line[i])
+        tmp['img'] = tmp['img'][:2]
+        tmp.pop('map')
         #img.append(tmp['img'])
         #intra.append(tmp['intra'])
         #bus.append(tmp['bus'])
         #map_1.append("http://api.map.baidu.com/staticimage?markers=" + urllib.quote(line[i]) + "&center=" + urllib.quote(line[i]))
         ret['name_' + str(i + 1)] = tmp
+
     map_3 = "http://api.map.baidu.com/staticimage?center=116.403874,39.914889&width=400&height=300&zoom=11&markers=116.288891,40.004261|116.487812,40.017524|116.525756,39.967111|116.536105,39.872374|116.442968,39.797022|116.270494,39.851993|116.275093,39.935251|116.383177,39.923743&markerStyles=l,A|m,B|l,C|l,D|m,E|,|l,G|m,H"
     ret['map'] = map_3
     #ret = {
@@ -50,6 +53,12 @@ def getLineInfo():
     #        'bus':bus
     #        }
     return json.dumps(ret)
+
+@app.route('/getPlace', methods = ['GET', 'POST'])
+def getPlaceInfo():
+    name = str(request.form['name'])
+    tmp = getInfo.getInfo(name)
+    return json.dumps(tmp)
 
 @app.route('/shakeLine', methods = ['GET', 'POST'])
 def oneday_api_shakeList():
